cd0600a6a31483b48deac57b5a0137b1c27a98bc
Jackson Ramos <jackramos021495@gmail.com>
2020-05-06 16:26:12 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -10,3 +10,16 @@ def sort_array_asc(array)
   end
 
 end
+
+def sort_array_asc(array)
+  array.sort do |a, b|
+    if a == b 
+      0
+    elsif a < b
+      1
+    elsif a > b 
+      -1
+    end
+  end
+end
+      
