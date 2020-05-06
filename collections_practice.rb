1cabf7541e120d20edcf80871bc519fc9fee46a3
Jackson Ramos <jackramos021495@gmail.com>
2020-05-06 16:32:20 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -27,12 +27,12 @@ def sort_array_char_count(array)
   array.sort do |a, b|
     if a.length == b.length
       0
-    elsif a.length < b.length 
+    elsif a.length < b.length
       -1
     elsif a.length > b.length
       1
     end
   end
 end
-      
+
       
